ca773597603b3ad006bf5679f29a2851c696eded
Lucas Goldner <lucas.goldner@googlemail.com>
2020-11-22 14:17:57 +0100

    Made a simple click counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angular-hello-world';
+  clickCounter = 0;
+
+  incrementCounter() {
+    this.clickCounter++;
+  }
 }

cb2707b200603dbf60ee0a99bb5f2b5d88d8edb0
Lucas Goldner <lucas.goldner@googlemail.com>
2020-11-22 15:19:12 +0100

    Working Currency Converter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ConverterComponent } from './converter/converter.component';
 import { AboutComponent } from './about/about.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConverterComponent implements OnInit {
 
+  amount = 1;
+  from = "EU";
+  to = "USD";
+  rate = 1.19; 
+
+  convert(): number{
+
+    return this.amount * this.rate;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
